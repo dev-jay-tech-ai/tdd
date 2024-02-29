7751cabe62789eda025ef59f15107c71d06db44d
jaewonks <jaewonaaart@gmail.com>
2024-02-29 15:09:12 +0000

    Unit test_getProducts

--- controller/products.js
+++ controller/products.js
@@ -7,7 +7,17 @@ exports.createProduct = async (req,res,next) => {
     res.status(201).json(createdProduct)
   } catch(error){ 
     console.log(error) 
-    next(error)  // 비동기 에러를 처리할 수 있는 곳으로 보내준다
+    next(error)  // 비동기 에러를 next를 이용해 애러를 처리할 수 있는 곳으로 보내준다
   } 
+}
+
+exports.getProducts = async (req,res,next) => {
+  try {
+    const allProducts = await productModel.find({})
+    res.status(200).json(allProducts)
+  } catch(error) {
+    next(error)
+  }
+
 
 }
--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ const productController = require('./controller/products')
 
 
 router.post('/',productController.createProduct)
+router.get('/',productController.getProducts)
 
 module.exports = router;
